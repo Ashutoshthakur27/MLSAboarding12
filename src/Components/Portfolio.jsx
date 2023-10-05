0b8b86568afaef6d12d7b403123f04ad12a2646b
Ashutoshthakur27 <95183260+Ashutoshthakur27@users.noreply.github.com>
2023-10-06 02:08:24 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/pic three.jpeg";
+import image from "../images/pic three.jpg";
 
 const imageAltText = "this is a background image";
 
